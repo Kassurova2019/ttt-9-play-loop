560411e18909810f32f976f1f130a1a5c8175612
Luba Kassurova <kassurova@icloud.com>
2022-01-08 15:07:12 -0500

    commit tree fc5a5c8372b9a14025587415f93fa53eb04dd79b

--- lib/play.rb
+++ lib/play.rb
@@ -1,4 +1,5 @@
-# Helper Methods
+# Helper Methods 
+require "pry"
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -36,3 +37,13 @@ def turn(board)
 end
 
 # Define your play method below
+
+def play(board)
+  counter = 0
+  
+  until counter == 9
+    binding.pry
+    counter += 1
+    turn(board)
+  end
+end
